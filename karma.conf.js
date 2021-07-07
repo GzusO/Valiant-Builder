74b4d58183fc533afb4be76c8209f02fd22d68a3
GzusO <garret.odegaard@gmail.com>
2021-07-06 20:59:58 -0500

    rename to Valiant Builder

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Character-Builder'),
+      dir: require('path').join(__dirname, './coverage/Valiant-Builder'),
       subdir: '.',
       reporters: [
         { type: 'html' },
