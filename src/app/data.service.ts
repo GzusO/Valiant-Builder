32d173c3d21c0a04808d5ed89c7a33a6010a5a4d
GzusO <garret.odegaard@gmail.com>
2021-07-15 21:32:34 -0500

    redo data service to map data on initialize instead of each function call

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -14,84 +14,64 @@ import { classData } from 'data/classes';
   providedIn: 'root'
 })
 export class DataService {
+  professions = professionData;
+  features = featureData; 
+  traits = traitData;
+  abilities = abilityData;
+  lineages = lineageData;
+  characteristics = characteristicData;
+  classes = classData;
+
   getProfessions(): Observable<Profession[]> {
-    const professions = professionData;
-    const features = featureData;
-    const traits = traitData;
-    const abilities = abilityData;
-
-    //Map traits to Features
-    features.map(x=>x.traits = (traits.filter(y => y.types.every(z=>x.types.includes(z)))!));
-    //Map abilties to Features
-    features.map(x=>x.abilities = (abilities.filter(y => y.types.every(z=>x.types.includes(z)))!));
-    //Map Features to Professions
-    professions.map(x =>x.features = (features.filter(y => y.types.includes(x.name))!));
-
-    return of(professions);
+    return of(this.professions);
   }
 
   getLineages(): Observable<Lineage[]>{
-    const lineages = lineageData;
-    const features = featureData;
-    const traits = traitData;
-    const abilities = abilityData;
-
-    //Map traits to Features
-    features.map(x=>x.traits = (traits.filter(y => y.types.includes(x.name))!));
-    //Map abilties to Features
-    features.map(x=>x.abilities = (abilities.filter(y => y.types.includes(x.name))!));
-
-
-    lineages.map(x => x.features = (features.filter(y => y.types.includes('Lineage')).filter(y => y.types.includes(x.name)))!);
-
-    return of(lineages);
+    return of(this.lineages);
   }
 
   getCharacteristics(): Observable<Characteristic[]>{
-    const characteristics = characteristicData;
-    const features =featureData;
-    const traits = traitData;
-    const abilities = abilityData;
-
-    //Map traits to Features
-    features.map(x=>x.traits = (traits.filter(y => y.types.includes(x.name))!));
-    //Map abilties to Features
-    features.map(x=>x.abilities = (abilities.filter(y => y.types.includes(x.name))!));
-
-    characteristics.map(x => x.features = (features.filter(y => y.types.includes(x.name))).sort((a,b) => a.tier-b.tier));
-
-    return of(characteristics);
+    return of(this.characteristics);
   }
 
   getClasses(): Observable<Class[]>{
-    const classes = classData;
-    const features= featureData;
-    const traits = traitData;
-    const abilities = abilityData;
-
-    //Map traits to Features
-    features.map(x=>x.traits = (traits.filter(y => y.types.includes(x.name))!));
-    //Map abilties to Features
-    features.map(x=>x.abilities = (abilities.filter(y => y.types.includes(x.name))!));
-
-    classes.map(x => x.features = (features.filter(y => y.types.includes(x.name))).sort((a,b) => a.tier-b.tier));
-
-    return of (classes);
+    return of (this.classes);
   }
 
   getFeaturesByType(type: string): Observable<Feature[]>{
-    const features = featureData;
-    const traits = traitData;
-    const abilities = abilityData;
-
-    const result = features.filter(x=> x.types.includes(type));
-    //Map traits to Features
-    result.map(x=>x.traits = (traits.filter(y => y.types.includes(x.name))!));
-    //Map abilties to Features
-    result.map(x=>x.abilities = (abilities.filter(y => y.types.includes(x.name))!));
-
-    return of(result);
+    return of(this.features.filter(x=> x.types.includes(type)));
   }
 
-  constructor() { }
+  getLineageFeatures(): Observable<Feature[]>{
+    return of(this.features.filter(x=> x.types.includes('Lineage')));
+  }
+
+
+  constructor() {
+    this.mapAbilities();
+    this.mapTraits();
+    this.mapCharacteristics();
+    this.mapProfessions();
+    this.mapClasses();
+    this.mapLineages();
+  }
+
+  mapAbilities(): void{
+    this.features.map(x=>x.abilities = (this.abilities.filter(y => y.types.includes(x.name))!));
+  }
+  mapTraits(): void{
+    this.features.map(x=>x.traits = (this.traits.filter(y => y.types.includes(x.name))!));
+  }
+  mapCharacteristics(): void{
+    this.characteristics.map(x => x.features = (this.features.filter(y => y.types.includes(x.name))).sort((a,b) => a.tier-b.tier));
+  }
+  mapProfessions(): void{
+    this.professions.map(x =>x.features = (this.features.filter(y => y.types.includes(x.name))!));
+  }
+  mapClasses(): void{
+    this.classes.map(x => x.features = (this.features.filter(y => y.types.includes(x.name))).sort((a,b) => a.tier-b.tier));
+  }
+  mapLineages(): void{
+    this.lineages.map(x => x.features = (this.features.filter(y => y.types.includes('Lineage')).filter(y => y.types.includes(x.name)))!);
+  }
 }
