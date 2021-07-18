8ecc0e2edf4f7d15c4e5437b481457217746a1ec
GzusO <garret.odegaard@gmail.com>
2021-07-17 19:10:22 -0500

    remove more unneed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -60,9 +59,6 @@ import { FeatureCardComponent } from './feature/feature-card/feature-card.compon
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, {dataEncapsulation: false, delay:0 }
-    ),
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
